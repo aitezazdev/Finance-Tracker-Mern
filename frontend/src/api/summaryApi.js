df51a81605aa0b7037af125132c752116b95826e
aitezazdev <aitezazsikandar@gmail.com>
2026-06-11 23:26:34 +0500

    fix: redirect to login page on expired JWT (401 status)

--- frontend/src/api/summaryApi.js
+++ frontend/src/api/summaryApi.js
@@ -1,19 +1,4 @@
-import axios from "axios"
-
-const API_URL = import.meta.env.VITE_API_URL;
-
-const api = axios.create({
-  baseURL: API_URL,
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+import api from "./client";
 
 // finanacial summary
 export const getFinancialSummary = async () => {
